f31d0cc07c8526c2869c3f34590b6b5f3f12e014
Kevin Eng <kevine1221@gmail.com>
2020-05-01 12:58:30 -0400

    added all data sets

---
+++ web-scraping/web-scrape.r
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(jsonlite)
+library(Rcurl)
+
+base_url <- "https://www.ncei.noaa.gov/access/services/data/v1"
+data_set <- "?dataset=daily-summaries"
+station_id <- "&stations=USW00094728"
+start_date <- "&startDate=2010-01-01"
+end_date <- "&endDate=2019-03-31"
+data_type <- "&dataTypes=TMAX,TMIN,PRCP,SNOW,AWND"
+units <- "&units=standard"
+
+url <- paste0(base_url,data_set,station_id,start_date,end_date,data_type,units)
+
+weather_data <- read_csv(url)
+write_csv(weather_data, "./data/weather-data.csv")
+
